bc6c9f6f703b7c8b4a599fce634d3f923094caef
Nhật Long Hồ <honhatlongg@gmail.com>
2021-06-02 17:10:47 +0700

    longHN

--- src/app/module/customer/customer.module.ts
+++ src/app/module/customer/customer.module.ts
@@ -2,8 +2,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ToastrModule} from 'ngx-toastr';
 import {CustomerPageComponent} from './customer-page/customer-page.component';
 import {ShowInfoComponent} from './customer-manager/show-info/show-info.component';
 import {EditInfoComponent} from './customer-manager/edit-info/edit-info.component';
@@ -13,7 +11,7 @@ import {CreatePostComponent} from './customer-post/create-post/create-post.compo
 import {DeletePostComponent} from './customer-post/delete-post/delete-post.component';
 import {EditPostComponent} from './customer-post/edit-post/edit-post.component';
 import {ListPostComponent} from './customer-post/list-post/list-post.component';
-import {RouterModule} from '@angular/router';
+import {CustomerRoutingModule} from "./customer-routing.module";
 
 @NgModule({
   declarations: [CustomerPageComponent, ShowInfoComponent, EditInfoComponent, ChangePasswordComponent,
@@ -23,9 +21,7 @@ import {RouterModule} from '@angular/router';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot(),
-    RouterModule,
+    CustomerRoutingModule,
   ]
 })
 export class CustomerModule {
